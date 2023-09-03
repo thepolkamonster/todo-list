9d1ba6d183025e7824412e370fb768b53719f1dd
thepolkamonster <sakshamparashar12@gmail.com>
2023-09-03 19:04:38 +0530

    Added route to add task through API, POST at /api/tasks

--- routes.py
+++ routes.py
@@ -3,22 +3,34 @@ from flask_smorest import Blueprint, abort
 from flask.views import MethodView
 from models import TaskModel, FinishedTaskModel
 from sqlalchemy.exc import SQLAlchemyError
-from schemas import TaskSchema
+from schemas import TaskSchema, TaskInputSchema
 
 blp = Blueprint("routes", __name__, description = "The api routes Blueprint")
 
 @blp.route("/api/tasks")
 class TaskRoute(MethodView):
-    @blp.response(200, TaskSchema(many = True))
 
+    @blp.response(200, TaskSchema(many = True))
     def get(self):
         res = TaskModel.query.all()
         return res
     
+
+    @blp.arguments(TaskInputSchema)
+    @blp.response(200, TaskSchema)
+    def post(self, TaskData):
+        temp_task = TaskModel(**TaskData)
+        try:
+            db.session.add(temp_task)
+            db.session.commit()
+        except SQLAlchemyError:
+            abort(500, message = "Error while inserting")
+        return temp_task
+    
 @blp.route("/api/finishedtasks")
 class FinishedTaskRoute(MethodView):
+
     @blp.response(200, TaskSchema(many = True))
-    
     def get(self):
         res = FinishedTaskModel.query.all()
         return res
--- schemas.py
+++ schemas.py
@@ -4,3 +4,6 @@ class TaskSchema(Schema):
     id = fields.Integer(dump_only = True)
     desc = fields.String(required=True)
     status = fields.String()
+
+class TaskInputSchema(Schema):
+    desc = fields.String(required=True)
